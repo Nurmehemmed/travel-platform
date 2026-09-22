786445394b2a4a394652cde10edf50431f57d966
nurmehemmed <nurmehemmedn@gmail.com>
2026-09-22 12:48:40 +0400

    refactor: remove custom image quality setting from next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -14,8 +14,6 @@ const nextConfig: NextConfig = {
     // Device breakpoints for responsive srcset
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    // Reduce quality slightly for faster loads (visually imperceptible)
-    quality: 80,
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
   },
 
